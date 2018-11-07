f1c9d7fbd87ccd5f00fe60691ea74f72cb1effa8
Gleb Amelichkin <glebamelichkin@gmail.com>
2018-11-07 16:41:47 +0200

    ravenous finale

---
+++ src/util/Yelp.js
@@ -0,0 +1,31 @@
+const apiKey = "cX75wTqm0uNFfZJFiIZiAO1eUQmvrQovUKRFAk9ipeuWojwXLKrGi3n4MzJx-x0GYLwcfmZo2lch7pmlICbi_079kQDuhTFxQ_lPZ4RFBK5HvrUn4XRnrC6CoC3gW3Yx";
+
+const Yelp = {
+  search(term, location, sortBy){
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+                 {headers: {
+                   Authorization: `Bearer ${apiKey}`
+                 }}).then(response=>{
+                   return response.json().then(jsonResponse=>{
+                     if (jsonResponse.businesses){
+                       return jsonResponse.businesses.map(business=>{
+                         return {
+                           id: business.id,
+                           imageSrc: business.image_url,
+                           name: business.name,
+                           adress: business.location.address1,
+                           city: business.location.city,
+                           state: business.location.state,
+                           zipCode: business.location.zip_code,
+                           category: business.categories[0].title,
+                           rating: business.rating,
+                           reviewCount: business.review_count
+                         }
+                       });
+                     }
+                   })
+                 })
+  }
+}
+
+export default Yelp;
